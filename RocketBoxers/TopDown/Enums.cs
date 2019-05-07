aedbe55f4fc8624beecdbd06dc5aaa489f104598
Victor Chelaru <VicChelaru@gmail.com>
2019-05-06 20:04:47 -0600

    First!

---
+++ RocketBoxers/TopDown/Enums.cs
@@ -0,0 +1,26 @@
+
+
+
+namespace RocketBoxers.Entities
+{
+    public enum TopDownDirection
+    {
+        Right = 0,
+        UpRight = 1,
+        Up = 2,
+        UpLeft = 3,
+        Left = 4,
+        DownLeft = 5,
+        Down = 6,
+        DownRight = 7
+    }
+
+    public enum PossibleDirections
+    {
+        LeftRight,
+        FourWay,
+        EightWay
+    }
+
+}
+
