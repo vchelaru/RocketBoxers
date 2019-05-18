db858576a1055d0d3131314a770c334c0c6f4929
Rick Blaylock <blay4480@gmail.com>
2019-05-17 22:43:51 -0600

    - Attack animations almost working. - Committing for a bug in the TopDown plugin. - Adding a movement type which prevents the players from moving during attacks or defense.

--- RocketBoxers/Game1.cs
+++ RocketBoxers/Game1.cs
@@ -60,7 +60,7 @@ namespace RocketBoxers
             var networkConfiguration = new GameNetworkConfiguration();
             RedGrin.NetworkManager.Self.Initialize(networkConfiguration, null);
 
-			FlatRedBall.Screens.ScreenManager.Start(typeof(RocketBoxers.Screens.TitleScreen));
+			FlatRedBall.Screens.ScreenManager.Start(typeof(RocketBoxers.Screens.TestLevel1));
 
 
             base.Initialize();
