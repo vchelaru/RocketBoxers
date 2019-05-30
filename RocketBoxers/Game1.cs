7d7d9c8003f4da3c82fd812891fdb9c1f2641d60
Alex Riak <acriak13@gmail.com>
2019-05-29 23:04:44 -0500

    Added the Menu (Title) Music to the Title Screen. Will hopefully get seamless into the Network Select and Player Select screen tomorrow.

--- RocketBoxers/Game1.cs
+++ RocketBoxers/Game1.cs
@@ -60,7 +60,7 @@ namespace RocketBoxers
             var networkConfiguration = new GameNetworkConfiguration();
             RedGrin.NetworkManager.Self.Initialize(networkConfiguration, null);
 
-			FlatRedBall.Screens.ScreenManager.Start(typeof(RocketBoxers.Screens.NetworkLobby));
+			FlatRedBall.Screens.ScreenManager.Start(typeof(RocketBoxers.Screens.TitleScreen));
 
 
             base.Initialize();
